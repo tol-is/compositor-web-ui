263bd00fea64768b3a6f9c588f797291bca26fc4
tol.is <me@tol.is>
2019-10-14 02:08:01 +0100

    oops

---
+++ src/BlackBox.js
@@ -0,0 +1,20 @@
+import { h } from 'preact';
+import { css } from 'emotion';
+
+export default props => {
+  return (
+    <div
+      className={css`
+        height: 64px;
+        min-width: 180px;
+        background-color: black;
+        color: white;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0 16px 0 16px;
+      `}
+      {...props}
+    />
+  );
+};
